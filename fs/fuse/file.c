e292cab993fa3273e7d180198f0578648d5a65b7
yazidkucrit <yazid.kucrit@gmail.com>
2022-01-31 09:31:12 +0700

    Update file.c

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2,7 +2,6 @@
   FUSE: Filesystem in Userspace
   Copyright (C) 2001-2008  Miklos Szeredi <miklos@szeredi.hu>
   Copyright (C) 2021 XiaoMi, Inc.
-
   This program can be distributed under the terms of the GNU GPL.
   See the file COPYING.
 */
@@ -212,7 +211,7 @@ int fuse_open_common(struct inode *inode, struct file *file, bool isdir)
 
 	if (fuse_is_bad(inode))
 		return -EIO;
-
+		
 	err = generic_file_open(inode, file);
 	if (err)
 		return err;
@@ -977,7 +976,7 @@ static ssize_t fuse_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 
 	if (fuse_is_bad(inode))
 		return -EIO;
-
+		
 	/*
 	 * In auto invalidate mode, always update attributes on read.
 	 * Otherwise, only update if we attempt to read past EOF (to ensure
@@ -1248,14 +1247,12 @@ static ssize_t fuse_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	struct inode *inode = mapping->host;
 	ssize_t err;
 	loff_t endbyte = 0;
-
-<<<<<<< HEAD
-	if (ff->passthrough.filp)
-		return fuse_passthrough_write_iter(iocb, from);
-=======
+	
 	if (fuse_is_bad(inode))
 		return -EIO;
->>>>>>> 2cd45139c0f2... fuse: fix bad inode
+
+	if (ff->passthrough.filp)
+		return fuse_passthrough_write_iter(iocb, from);
 
 	if (get_fuse_conn(inode)->writeback_cache) {
 		/* Update size (EOF optimization) and mode (SUID clearing) */
